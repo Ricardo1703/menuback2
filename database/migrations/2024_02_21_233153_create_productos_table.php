1305b057d10811bf967fd5be656da985ee5f1efb
Ricardo1703 <66895949+Ricardo1703@users.noreply.github.com>
2024-02-28 15:16:04 -0600

    unsigned user_id

--- database/migrations/2024_02_21_233153_create_productos_table.php
+++ database/migrations/2024_02_21_233153_create_productos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('name', 45);
             $table->string('descripcion', 150);
             $table->decimal('precio', 8, 2);
